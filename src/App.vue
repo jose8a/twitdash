7eb2ec7ee13a7b891c8b56875fd633e4c6c0f83d
jose8a <ochovilla@gmail.com>
2017-08-11 11:20:38 -0700

    Initial commit - skeleton app with webpack setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <nav-header></nav-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavHeader from './components/NavHeader';
+import User from './components/User';
+
+export default {
+  name: 'app',
+  components: {
+    NavHeader,
+    User,
+  },
+};
+</script>
+
+<style>
+</style>
