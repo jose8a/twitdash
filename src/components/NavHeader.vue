ae1149ceda10fa3a6e21de09c9446fc6474e8e61
jose8a <ochovilla@gmail.com>
2017-08-11 12:11:45 -0700

    Add NavHeader module.

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id='nav'>
+      <router-link to='/lists' tag="button">Lists</router-link>
+      <router-link to='/lists/favorites' tag="button">Favorites</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-header',
+};
+</script>
+
+<style>
+</style>
