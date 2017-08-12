75a452aaf6b5a3f29c623d53e3bbab85e1c86c44
jose8a <ochovilla@gmail.com>
2017-08-11 21:19:51 -0700

    Add status bar with status of currently-loaded 'page'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <nav-header></nav-header>
+    <h3>{{ getPath }}</h3>
     <router-view></router-view>
   </div>
 </template>
@@ -12,6 +13,18 @@ export default {
   name: 'app',
   components: {
     NavHeader,
+  },
+  computed: {
+    getPath() {
+      // --- return this.$route.path;
+      if (this.$route.path === '/') {
+        return 'Home';
+      } else if (this.$route.path === '/lists') {
+        return 'Lists';
+      }
+
+      return 'Timeline';
+    },
   },
 };
 </script>
